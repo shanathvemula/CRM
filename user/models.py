ccdd5365f4766f8095b58df2720ae7cb701045ba
shanathvemula <shanathvemula@panfx.com>
2024-03-27 08:33:24 +0530

    update code

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User, Group, Permission
 # Create your models here.
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    building =models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    state = models.CharField(max_length=250)
+    city = models.CharField(max_length=250)
+    deleted = models.BooleanField(default=False)
